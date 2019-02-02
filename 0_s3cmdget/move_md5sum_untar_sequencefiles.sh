44625f86102e985eec351cb3a49e8fffaa43be6f
dougwyu <dougwyu@gmail.com>
2019-02-02 13:56:53 +0000

    change ~/ArcDyn/ to ~/_ArcDyn/

--- 0_s3cmdget/move_md5sum_untar_sequencefiles.sh
+++ 0_s3cmdget/move_md5sum_untar_sequencefiles.sh
@@ -13,7 +13,7 @@ set -o pipefail
 
 ssh hpc
 interactive
-cd ~/ArcDyn/
+cd ~/_ArcDyn/
 ls
 
 # checking on nohup jobs
@@ -24,7 +24,7 @@ ls
 # md5sums from Amazon Glacier
 # 2016-09-20 20:54 425241579520   55597861df107930ae4a881b0e72d288  s3://amazon-glacier-douglasyu/Earlham_soups_20160910.tar
 
-cd ~/ArcDyn/
+cd ~/_ArcDyn/
 
 # mv PlatesAB sequence files to PlatesAB/, run in another window
 mv Earlham_soups_20160910.tar PlatesAB/
@@ -35,10 +35,10 @@ nohup md5sum Earlham_soups_20160910.tar > PlatesAB.md5 & # run in separate windo
 # takes a long time
 nohup tar -xvf Earlham_soups_20160910.tar & # run in separate window
 
-cd ~/ArcDyn/PlatesAB; ls
+cd ~/_ArcDyn/PlatesAB; ls
 cat PlatesAB.md5 # it matches, so i can untar and start working on it
 # 55597861df107930ae4a881b0e72d288  Earlham_soups_20160910.tar
-rm -rf ~/ArcDyn/PlatesAB/Earlham_soups/f16a1625-300b-4746-bfa3-61bbb36634d6/Sample_IPO3916_F5_working/ # there is a weird folder with very small files in there. Sample_IPO3916_F5/ is also present, also with small files. This is a failed sample
+rm -rf ~/_ArcDyn/PlatesAB/Earlham_soups/f16a1625-300b-4746-bfa3-61bbb36634d6/Sample_IPO3916_F5_working/ # there is a weird folder with very small files in there. Sample_IPO3916_F5/ is also present, also with small files. This is a failed sample
 
 ################ PlatesA2B2
 # md5sums from Amazon Glacier
@@ -46,7 +46,7 @@ rm -rf ~/ArcDyn/PlatesAB/Earlham_soups/f16a1625-300b-4746-bfa3-61bbb36634d6/Samp
 # 2018-01-24 00:25 217650309120   67c93451465fa5afb0cc341ae6a47c17  s3://amazon-glacier-douglasyu/PlatesA2B2_PKG-ENQ-1643-Data_Transfer-PSEQ-1618_8_lanes.tar
 # 2018-02-21 19:12 24986681797   bf9cc26c860c52b8ddd26fe2e638cd6a  s3://amazon-glacier-douglasyu/PlatesA2B2_PKG-ENQ-1643-Data_Transfer-PSEQ-1657_1_lane.tar.gz
 
-cd ~/ArcDyn/
+cd ~/_ArcDyn/
 
 # mv PlatesA2B2 sequence files to PlatesA2B2/, run in another window
 mv PlatesA2B2*.tar* PlatesA2B2/
@@ -59,7 +59,7 @@ nohup tar -xvf PlatesA2B2_PKG-ENQ-1643-Data_Transfer-PSEQ-1600_2_lanes.tar &
 nohup tar -xvf PlatesA2B2_PKG-ENQ-1643-Data_Transfer-PSEQ-1618_8_lanes.tar &
 nohup tar -xzvf PlatesA2B2_PKG-ENQ-1643-Data_Transfer-PSEQ-1657_1_lane.tar.gz &
 
-cd ~/ArcDyn/PlatesA2B2; ls
+cd ~/_ArcDyn/PlatesA2B2; ls
 mv PKG-ENQ-1643-Data_Transfer-PSEQ-1657_1_lane PKG-ENQ-1643-Data_Transfer-PSEQ-1657_1_lanes # add an 's' to the end of the folder name so that it is easier to grob in the next script
 ls PKG-ENQ-1643-Data_Transfer-PSEQ-1657_1_lanes/ | less # check
 cat PlatesA2B2.md5 # it matches, so i can untar and start working on it
@@ -72,7 +72,7 @@ cat PlatesA2B2.md5 # it matches, so i can untar and start working on it
 # md5sums from Amazon Glacier
 # 2017-06-03 18:06 450506629120   6f73bd9257bb54695a7c72c575b3a599  s3://amazon-glacier-douglasyu/Earlham_soups_20170603.tar
 
-cd ~/ArcDyn/
+cd ~/_ArcDyn/
 # mv PlatesEF sequence files to PlatesEF/, run in another window
 mv Earlham_soups_20170603.tar PlatesEF/
 ls
@@ -82,7 +82,7 @@ nohup md5sum Earlham_soups_20170603.tar > PlatesEF.md5 & # PID 17526
 # extract the files, takes a long time, run in separate window
 nohup tar -xvf Earlham_soups_20170603.tar & # PID 17527
 
-cd ~/ArcDyn/PlatesEF; ls
+cd ~/_ArcDyn/PlatesEF; ls
 cat PlatesEF.md5 # it matches, so i can untar and start working on it
 # 6f73bd9257bb54695a7c72c575b3a599  Earlham_soups_20170603.tar
 
@@ -93,9 +93,10 @@ cat PlatesEF.md5 # it matches, so i can untar and start working on it
 # 2018-03-01 13:20 186389913600   0167c4725ff9ac50390127cc6579118c  s3://amazon-glacier-douglasyu/PlatesGH_PKG-ENQ-2379-Data_Transfer-PSEQ-1600.tar
 # 2018-03-01 15:10 90482073600   e1f1efea1c1feca0976b7df232804c22  s3://amazon-glacier-douglasyu/PlatesGH_PKG-ENQ-2379-Data_Transfer-PSEQ-1603.tar
 
-cd ~/ArcDyn/
+cd ~/_ArcDyn/
 
 # mv PlatesGH sequence files to PlatesGH/
+# mkdir PlatesGH/
 mv PlatesGH*.tar PlatesGH/
 ls
 cd PlatesGH/; ls
@@ -116,7 +117,7 @@ ps -ef | grep tar # shows PIDs with word 'tar' in them (thus, picks up both md5s
 
 
 ################ mv the Amazon Glacier-downloaded tar and md5 files to a folder for temp holding
-# cd ~/ArcDyn/
+# cd ~/_ArcDyn/
 # mkdir Amazon_archive_files_to_rm_at_end/
 # mv PlatesEF/Earlham_soups_20170603.tar Amazon_archive_files_to_rm_at_end/; ls Amazon_archive_files_to_rm_at_end/
 # mv PlatesAB/Earlham_soups_20160910.tar Amazon_archive_files_to_rm_at_end/
@@ -126,6 +127,6 @@ ps -ef | grep tar # shows PIDs with word 'tar' in them (thus, picks up both md5s
 # mv PlatesGH/PlatesGH_PKG-ENQ-2379-Data_Transfer-PSEQ-1586-trimmed.tar Amazon_archive_files_to_rm_at_end/
 # mv PlatesGH/PlatesGH_PKG-ENQ-2379-Data_Transfer-PSEQ-1600.tar Amazon_archive_files_to_rm_at_end/
 # mv PlatesGH/PlatesGH_PKG-ENQ-2379-Data_Transfer-PSEQ-1603.tar Amazon_archive_files_to_rm_at_end/
-# mv ~/ArcDyn/amazon-glacier-douglasyu20190113.md5 ~/ArcDyn/Amazon_archive_files_to_rm_at_end/
-# mv ~/ArcDyn/Plates*/Plates*.md5 ~/ArcDyn/Amazon_archive_files_to_rm_at_end/
-ls ~/ArcDyn/Amazon_archive_files_to_rm_at_end
+# mv ~/_ArcDyn/amazon-glacier-douglasyu20190113.md5 ~/_ArcDyn/Amazon_archive_files_to_rm_at_end/
+# mv ~/_ArcDyn/Plates*/Plates*.md5 ~/_ArcDyn/Amazon_archive_files_to_rm_at_end/
+ls ~/_ArcDyn/Amazon_archive_files_to_rm_at_end
--- 4_minimap2_samtools/launch_postsamtools_copy_idx_genomecov_files.sh
+++ 4_minimap2_samtools/launch_postsamtools_copy_idx_genomecov_files.sh
@@ -14,60 +14,62 @@ interactive
 # to use parallel without a pathname in bsub scripts
 PATH=$PATH:~/scripts/parallel-20170722/bin/
 
-# FILTER="F2308"
 OUTPUTFOLDER="minimap2_outputs"
 
 ####### cd into different folders and set up output folders to hold everything before running
 ####### the generic copy script below
 # A2B2
 PLATE="A2B2"
-WORKINGPATH="$HOME/ArcDyn/Plates${PLATE}/Plates${PLATE}_combined/"
+WORKINGPATH="$HOME/_ArcDyn/Plates${PLATE}/Plates${PLATE}_combined/"
 cd ${WORKINGPATH}
 ls
 mkdir ${OUTPUTFOLDER}_Plates${PLATE}/
 ls
 ls ${OUTPUTFOLDER}_Plates${PLATE}/
-# ls BWA*/*.out # don't know what this is for
+"ls" -l BWA*/s*${PLATE}*.out # check if all BWA folders have a current smtl*.out file, showing that samtools ran correctly.  Check the sizes of the samtools.out file.  they should all be about the same.
 ####### THEN JUMP DOWN AND RUN THE GENERIC CODE below
 
 
 
 # AB # different scripts because the pathnames are different
 PLATE="AB"
-WORKINGPATH="$HOME/ArcDyn/Plates${PLATE}/Plates${PLATE}_combined/"
+WORKINGPATH="$HOME/_ArcDyn/Plates${PLATE}/Plates${PLATE}_combined/"
 cd ${WORKINGPATH}
 ls
 mkdir ${OUTPUTFOLDER}_Plates${PLATE}/
 ls
 ls ${OUTPUTFOLDER}_Plates${PLATE}/
+"ls" -l BWA*/s*${PLATE}*.out # check if all BWA folders have a current smtl*.out file, showing that samtools ran correctly.  Check the sizes of the samtools.out file.  they should all be about the same.
 ####### THEN JUMP DOWN AND RUN THE GENERIC CODE below
 
 
 
 # EF
 PLATE="EF"
-WORKINGPATH="$HOME/ArcDyn/Plates${PLATE}/Plates${PLATE}_combined/"
+WORKINGPATH="$HOME/_ArcDyn/Plates${PLATE}/Plates${PLATE}_combined/"
 cd ${WORKINGPATH}
 ls
 mkdir ${OUTPUTFOLDER}_Plates${PLATE}/
 ls
 ls ${OUTPUTFOLDER}_Plates${PLATE}/
+"ls" -l BWA*/s*${PLATE}*.out # check if all BWA folders have a current smtl*.out file, showing that samtools ran correctly.  Check the sizes of the samtools.out file.  they should all be about the same.
 ####### THEN JUMP DOWN AND RUN THE GENERIC CODE below
 
 
 
 # GH
 PLATE="GH"
-WORKINGPATH="$HOME/ArcDyn/Plates${PLATE}/Plates${PLATE}_combined/"
+WORKINGPATH="$HOME/_ArcDyn/Plates${PLATE}/Plates${PLATE}_combined/"
 cd ${WORKINGPATH}
 ls
 mkdir ${OUTPUTFOLDER}_Plates${PLATE}/
 ls
 ls ${OUTPUTFOLDER}_Plates${PLATE}/
+"ls" -l BWA*/s*${PLATE}*.out # check if all BWA folders have a current smtl*.out file, showing that samtools ran correctly.  Check the sizes of the samtools.out file.  they should all be about the same.
 ####### THEN JUMP DOWN AND RUN THE GENERIC CODE below
 
 
-####### GENERIC CODE to copy samtools output files to folder:  bwa_all_outputs_Plates${PLATE}/ #######
+####### GENERIC CODE to copy samtools output files to:  outputs_Plates${PLATE}_${FILTER1}_q${QUAL2}_${OUTPUTFOLDER}_${MAPDATE}_${TARGET}.tar.gz #######
 ####### This folder is then downloaded to my laptop to process with R:  idxstats_tabulate_macOS_Plates*.Rmd
 ####### Run once for each library:  A2B2, AB, EF, GH
 
@@ -82,11 +84,11 @@ QUAL2=48; echo $QUAL2
 echo $PLATE # check that i'm going to the right folder
 echo $OUTPUTFOLDER
 
-# copy output files into bwa_all_outputs_Plates${PLATE}/
+# copy output files into minimap2_outputs_Plates${PLATE}/
 # e.g. Sample_IPO3916_C5_F2308_f0x2_q60_sorted.bam_idxstats.txt
 # check before copying
 parallel ls -l BWA*/${OUTPUTFOLDER}/*_{1}_q{2}_sorted.bam_idxstats.txt ::: ${FILTER1} ${FILTER2} ::: ${QUAL1} ${QUAL2}
-parallel ls BWA*/${OUTPUTFOLDER}/*_{1}_q{2}_sorted.bam_idxstats.txt ::: ${FILTER1} ${FILTER2} ::: ${QUAL1} ${QUAL2} | wc -l # A2B2: 342 files == 171*2; AB: 384; EF: 384 files == 192*2; GH: 380;
+parallel ls BWA*/${OUTPUTFOLDER}/*_{1}_q{2}_sorted.bam_idxstats.txt ::: ${FILTER1} ${FILTER2} ::: ${QUAL1} ${QUAL2} | wc -l # A2B2: 342 files == 171*2; AB: 384 == 192*2; EF: 384 files; GH: 380;
 # copy
 parallel cp BWA*/${OUTPUTFOLDER}/*_{1}_q{2}_sorted.bam_idxstats.txt ${OUTPUTFOLDER}_Plates${PLATE}/ ::: ${FILTER1} ${FILTER2} ::: ${QUAL1} ${QUAL2}
 # check after copying
@@ -102,84 +104,79 @@ ls ${OUTPUTFOLDER}_Plates${PLATE}/*_sorted.bam.flagstat.txt | wc -l # half the i
 parallel ls -l BWA*/${OUTPUTFOLDER}/*_{1}_q{2}_sorted_genomecov_d.txt.gz ::: ${FILTER1} ${FILTER2} ::: ${QUAL1} ${QUAL2}
 parallel ls -l BWA*/${OUTPUTFOLDER}/*_{1}_q{2}_sorted_genomecov_d.txt.gz ::: ${FILTER1} ${FILTER2} ::: ${QUAL1} ${QUAL2} | wc -l # A2B2: 342; AB: 384; EF: 384; GH: 380;
 # copy
-parallel cp BWA*/${OUTPUTFOLDER}/*_{1}_q{2}_sorted_genomecov_d.txt.gz ${OUTPUTFOLDER}_Plates${PLATE}/ ::: ${FILTER1} ${FILTER2} ::: ${QUAL1} ${QUAL2}  # takes longer than above
+parallel cp BWA*/${OUTPUTFOLDER}/*_{1}_q{2}_sorted_genomecov_d.txt.gz ${OUTPUTFOLDER}_Plates${PLATE}/ ::: ${FILTER1} ${FILTER2} ::: ${QUAL1} ${QUAL2}  # takes considerably longer than above
 # check again
 parallel ls ${OUTPUTFOLDER}_Plates${PLATE}/*_{1}_q{2}_sorted_genomecov_d.txt.gz ::: ${FILTER1} ${FILTER2} ::: ${QUAL1} ${QUAL2} | wc -l # A2B2: 342; AB: 384; EF: 384; GH: 380;
-ls ${OUTPUTFOLDER}_Plates${PLATE}/*genomecov_d.txt.gz
-ls ${OUTPUTFOLDER}_Plates${PLATE}/*genomecov_d.txt.gz | wc -l # A2B2: 342; AB: 384; EF: 384; GH: 380;
 
 # rename, tar, and gzip for download
 MAPDATE="20190202"
-TARGET="406barcodes" # "308mitogenomes" or "406barcodes"
-du -sh ${OUTPUTFOLDER}_Plates${PLATE}/ # ~4.4 GB
+TARGET="349MITOCOICYTB" # "308mitogenomes", "406barcodes", "349MTCOICYTB"
+du -sh ${OUTPUTFOLDER}_Plates${PLATE}/ # ~4.5-5.0 GB
 # set filename to something that i can understand after download
 mv ${OUTPUTFOLDER}_Plates${PLATE} outputs_Plates${PLATE}_${FILTER1}_q${QUAL2}_${OUTPUTFOLDER}_${MAPDATE}_${TARGET}
 ls # e.g. outputs_PlatesA2B2_F2308_f0x2_q48_minimap2_outputs_20190115_308mitogenomes/
 # tar gzip for download
 tar -czvf outputs_Plates${PLATE}_${FILTER1}_q${QUAL2}_${OUTPUTFOLDER}_${MAPDATE}_${TARGET}.tar.gz outputs_Plates${PLATE}_${FILTER1}_q${QUAL2}_${OUTPUTFOLDER}_${MAPDATE}_${TARGET}/
-# # format:  outputs_PlatesA2B2_F2308_f0x2_q48_minimap2_outputs_20190115_308mitogenomes.tar.gz
+# filename format:  outputs_PlatesA2B2_F2308_f0x2_q48_minimap2_outputs_20190115_308mitogenomes.tar.gz
 ls
 rm -rf outputs_Plates${PLATE}_${FILTER1}_q${QUAL2}_${OUTPUTFOLDER}_${MAPDATE}_${TARGET}/
 ls
 
-# code needs to be made more robust and checked before running
-# remove the minimap2_output files after i've finished the mapping jobs. these are the bam, bam.bai, idxstats, flagstats, and genomecov files. Should save
-# parallel "ls BWA{}/minimap2_outputs/" ::: 01 02 03 04 05 06 07 08 09 10
-# parallel "rm -rf BWA{}/minimap2_outputs/" ::: 01 02 03 04 05 06 07 08 09 10
-
-######## remove the minimap2_output files after i've finished the mapping jobs. these are the bam, bam.bai, idxstats, flagstats, and genomecov files. Should save
-# to use parallel without a pathname in bsub scripts
+######## remove the minimap2_output/ folders after i've finished the mapping jobs. these are the bam, bam.bai, idxstats, flagstats, and genomecov files left behind in the BWA folders
 PATH=$PATH:~/scripts/parallel-20170722/bin/
 # FILTER="F2308"
 OUTPUTFOLDER="minimap2_outputs"
 
 # A2B2
 PLATE="A2B2"
-WORKINGPATH="$HOME/ArcDyn/Plates${PLATE}/Plates${PLATE}_combined/"
+WORKINGPATH="$HOME/_ArcDyn/Plates${PLATE}/Plates${PLATE}_combined/"
 cd ${WORKINGPATH}
 ls
 parallel "ls BWA{}/minimap2_outputs/" ::: 01 02 03 04 05 06 07 08 09 10
 # parallel "rm -rf BWA{}/minimap2_outputs/" ::: 01 02 03 04 05 06 07 08 09 10
 parallel "ls BWA{}/minimap2_outputs/" ::: 01 02 03 04 05 06 07 08 09 10
+# ls: cannot access BWA*/minimap2_outputs/: No such file or directory
 
 # AB # different scripts because the pathnames are different
 PLATE="AB"
-WORKINGPATH="$HOME/ArcDyn/Plates${PLATE}/Plates${PLATE}_combined/"
+WORKINGPATH="$HOME/_ArcDyn/Plates${PLATE}/Plates${PLATE}_combined/"
 cd ${WORKINGPATH}
 ls
 parallel "ls BWA{}/minimap2_outputs/" ::: 01 02 03 04 05 06 07 08 09 10
 # parallel "rm -rf BWA{}/minimap2_outputs/" ::: 01 02 03 04 05 06 07 08 09 10
 parallel "ls BWA{}/minimap2_outputs/" ::: 01 02 03 04 05 06 07 08 09 10
-
+# ls: cannot access BWA*/minimap2_outputs/: No such file or directory
 
 # EF
 PLATE="EF"
-WORKINGPATH="$HOME/ArcDyn/Plates${PLATE}/Plates${PLATE}_combined/"
+WORKINGPATH="$HOME/_ArcDyn/Plates${PLATE}/Plates${PLATE}_combined/"
 cd ${WORKINGPATH}
 ls
 parallel "ls BWA{}/minimap2_outputs/" ::: 01 02 03 04 05 06 07 08 09 10
 # parallel "rm -rf BWA{}/minimap2_outputs/" ::: 01 02 03 04 05 06 07 08 09 10
 parallel "ls BWA{}/minimap2_outputs/" ::: 01 02 03 04 05 06 07 08 09 10
+# ls: cannot access BWA*/minimap2_outputs/: No such file or directory
 
 
 # GH
 PLATE="GH"
-WORKINGPATH="$HOME/ArcDyn/Plates${PLATE}/Plates${PLATE}_combined/"
+WORKINGPATH="$HOME/_ArcDyn/Plates${PLATE}/Plates${PLATE}_combined/"
 cd ${WORKINGPATH}
 ls
 parallel "ls BWA{}/minimap2_outputs/" ::: 01 02 03 04 05 06 07 08 09 10
 # parallel "rm -rf BWA{}/minimap2_outputs/" ::: 01 02 03 04 05 06 07 08 09 10
 parallel "ls BWA{}/minimap2_outputs/" ::: 01 02 03 04 05 06 07 08 09 10
+# ls: cannot access BWA*/minimap2_outputs/: No such file or directory
 
 
 # when i'm on a fast network, i can download using scp, but otherwise, use Transmit for robustness
 ### 2016 run:  AB ###
 # on macOS
-# download bwa_all_outputs_PlatesAB/
-# scp b042@hpc.uea.ac.uk:~/ArcDyn/PlatesAB/PlatesAB_combined/bwa_all_outputs_Plates${PLATE}_${FILTER}.tar.gz  ~/Dropbox/Working_docs/Roslin_Greenland/2016/bulk_samples/PlatesAB_EI_20160512/
+# download outputs_Plates${PLATE}_${FILTER1}_q${QUAL2}_${OUTPUTFOLDER}_${MAPDATE}_${TARGET}.tar.gz
+# scp b042@hpc.uea.ac.uk:~/_ArcDyn/PlatesAB/PlatesAB_combined/outputs_Plates${PLATE}_${FILTER1}_q${QUAL2}_${OUTPUTFOLDER}_${MAPDATE}_${TARGET}.tar.gz  ~/Dropbox/Working_docs/Roslin_Greenland/2016/bulk_samples/PlatesAB_EI_20160512/
 #
 # cd /Users/Negorashi2011/Dropbox/Working_docs/Roslin_Greenland/2016/bulk_samples/PlatesAB_EI_20160512/
 #
-# scp b042@hpc.uea.ac.uk:~/ArcDyn/platesEF/PlatesEF_combined/fastqc_completed/bwa_all_outputs_PlatesEF_F2308_COIBarcode.tar.gz  ~/Dropbox/Working_docs/Roslin_Greenland/2017/bulk_samples/PlatesEF/
+# scp b042@hpc.uea.ac.uk:~/_ArcDyn/platesEF/PlatesEF_combined/fastqc_completed/bwa_all_outputs_PlatesEF_F2308_COIBarcode.tar.gz  ~/Dropbox/Working_docs/Roslin_Greenland/2017/bulk_samples/PlatesEF/
 #
 # cd ~/Dropbox/Working_docs/Roslin_Greenland/2017/bulk_samples/PlatesEF/
